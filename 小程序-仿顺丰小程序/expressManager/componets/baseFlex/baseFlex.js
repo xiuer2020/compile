46181c97bf2ed0cd650db37d490ebf5d76c79224
userName <xiuer_csdn@163.com>
2020-11-21 22:07:18 +0800

    首次完成

---
+++ 小程序-仿顺丰小程序/expressManager/componets/baseFlex/baseFlex.js
@@ -0,0 +1,33 @@
+// componets/baseFlex.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    jc: {
+      type: String,
+      value: "jcfs"
+    },
+    fxw: {
+      type: String,
+      value: "fxww"
+    },
+    ai: {
+      type: String,
+      value: "aic"
+    },
+    supplement: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {}
+})
